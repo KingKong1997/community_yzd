9d24865a53705af6067daccc3cfdbe684e3d5d58
yzd <1600849281@qq.com>
2020-04-23 17:23:36 +0800

    init repo

---
+++ src/main/java/com/zsc/commuity_yzd/CommuityYzdApplication.java
@@ -0,0 +1,13 @@
+package com.zsc.commuity_yzd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuityYzdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommuityYzdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsc/commuity_yzd/CommuityYzdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsc.commuity_yzd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuityYzdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
